c8690278a5aa053cdf3caa8e80f862744016eeaf
misterfish <allen@netherrealm.net>
2016-06-04 04:22:18 +0200

    update util spec

--- spec/utilSpec.js
+++ spec/utilSpec.js
@@ -46,7 +46,89 @@ describe('Util', function(){
   test('chr 90 -> Z', function(){
     return expect(tgt.chr(90)).toEqual('Z');
   });
-  return test('chr 947 -> γ', function(){
+  test('chr 947 -> γ', function(){
     return expect(tgt.chr(947)).toEqual('γ');
+  });
+  test('range', function(){
+    var cnt, val;
+    cnt = 0;
+    val = 0;
+    tgt.range(3, 7, function(n){
+      cnt = cnt + 1;
+      return val = val + n;
+    });
+    expect(cnt).toEqual(5);
+    return expect(val).toEqual(25);
+  });
+  test('range invalid', function(){
+    var threw, e;
+    threw = void 8;
+    try {
+      tgt.range('abc', 1);
+    } catch (e$) {
+      e = e$;
+      threw = true;
+    }
+    expect(threw).toEqual(true);
+    threw = void 8;
+    try {
+      tgt.range(4, 6.5);
+    } catch (e$) {
+      e = e$;
+      threw = true;
+    }
+    return expect(threw).toEqual(true);
+  });
+  test('times', function(){
+    var cnt, val;
+    cnt = 0;
+    val = 0;
+    tgt.times(4, function(n){
+      cnt = cnt + 1;
+      return val = val + n;
+    });
+    expect(cnt).toEqual(4);
+    return expect(val).toEqual(6);
+  });
+  test('times invalid', function(){
+    var threw, e;
+    threw = void 8;
+    try {
+      tgt.times(-3);
+    } catch (e$) {
+      e = e$;
+      threw = true;
+    }
+    return expect(threw).toEqual(true);
+  });
+  test('array', function(){
+    return expect(tgt.array(1, 5, 'a', {
+      b: 12
+    })).toEqual([
+      1, 5, 'a', {
+        b: 12
+      }
+    ]);
+  });
+  test('to-array', function(){
+    var fun;
+    fun = function(a, b, c){
+      return expect(tgt.toArray(arguments)).toEqual([10, 20, 30]);
+    };
+    return fun(10, 20, 30);
+  });
+  test('flat-array 1', function(){
+    return expect(tgt.flatArray(1, [1, 2], [1, [2, 3]])).toEqual([1, 1, 2, 1, 2, 3]);
+  });
+  test('flat-array 2', function(){
+    return expect(tgt.flatArray(1, 2, 3, 4, 5)).toEqual([1, 2, 3, 4, 5]);
+  });
+  return test('flat-array 3', function(){
+    var obj;
+    obj = {
+      a: 1,
+      b: 2
+    };
+    return expect(tgt.flatArray([[obj, obj], obj], obj, [obj])).toEqual([obj, obj, obj, obj, obj]);
   });
 });
--- spec/utilSpec.ls
+++ spec/utilSpec.ls
@@ -36,3 +36,77 @@ describe 'Util' ->
         expect tgt.chr 90 .to-equal 'Z'
     test 'chr 947 -> γ' ->
         expect tgt.chr 947 .to-equal 'γ'
+
+    test 'range' ->
+        cnt = 0
+        val = 0
+        tgt.range 3 7 (n) ->
+            cnt := cnt + 1
+            val := val + n
+        expect cnt .to-equal 5
+        expect val .to-equal 25
+
+    test 'range invalid' ->
+        threw = void
+        try
+            tgt.range 'abc' 1
+        catch
+            threw = true
+        expect threw .to-equal true
+
+        threw = void
+        try
+            tgt.range 4 6.5
+        catch
+            threw = true
+        expect threw .to-equal true
+
+    test 'times' ->
+        cnt = 0
+        val = 0
+        tgt.times 4 (n) ->
+            cnt := cnt + 1
+            val := val + n
+        expect cnt .to-equal 4
+        expect val .to-equal 6
+
+    test 'times invalid' ->
+        threw = void
+        try
+            tgt.times -3
+        catch
+            threw = true
+        expect threw .to-equal true
+
+    test 'array' ->
+        expect tgt.array 1 5 'a' b: 12 .to-equal [
+            1 5 'a' b: 12
+        ]
+
+    test 'to-array' ->
+        fun = (a, b, c) ->
+            expect tgt.to-array & .to-equal [10 20 30]
+        fun 10 20 30
+
+    test 'flat-array 1' ->
+        expect tgt.flat-array do
+            1
+            [1 2]
+            [1 [2 3]]
+        .to-equal do
+            [1 1 2 1 2 3]
+
+    test 'flat-array 2' ->
+        expect tgt.flat-array do
+            1 2 3 4 5
+        .to-equal do
+            [1 2 3 4 5]
+
+    test 'flat-array 3' ->
+        obj = a:1 b:2
+        expect tgt.flat-array do
+            [[obj, obj], obj]
+            obj
+            [obj]
+        .to-equal do
+            [obj] * 5
